9803a2759a6d5c8a2bbed04e8ffb7bbdb6924955
Rachid Elouali <rachidelouali@mbp-van-rachid.home>
2023-05-20 22:15:23 +0200

    Connecting server-side to client

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,71 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const dotenv = require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+const bodyParser = require("body-parser");
 
-// Home page rendering
-app.get("/", function (req, res) {
-  const items = ["item 1", "item 2", "item 3"];
-  ejs.renderFile("views/home.ejs", { items: items }, function (err, html) {
-    // Error handling for rendering
-    if (err) {
-      console.log(err);
-      res.status(500).send("Error rendering template");
-    } else {
-      res.send(html);
-    }
-  });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+console.log("Connected to MongoDB");
+
+// Enable CORS (Cross-Origin Resource Sharing)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// connecting back-end
+app.use(bodyParser.json());
+
+app.post("/submit-date-range", (req, res) => {
+  const dateRange = req.body; // Access the date range sent in the request body
+  console.log("Received date range:", dateRange);
+
+  // Perform any necessary actions with the date range data
+
+  res.send("Success"); // Send a response back to the client
+});
+
+// Homepage rendering
+app.get("", (req, res) => {
+  res.send("This is the homepage");
 });
 
 app.get("/about", (req, res) => {
@@ -29,6 +81,6 @@ app.use((req, res) => {
 app.use(express.static("static"));
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 4123, () => {
+  console.log("Server started on port 4123");
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,61 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
 import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme();
 
 export default function BasicDateRangeCalendar() {
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [serverResponse, setServerResponse] = useState("");
+
+  const handleDateRangeSelect = (range) => {
+    setDateRange(range);
+  };
+
+  const handleButtonClick = () => {
+    fetch("http://localhost:4123/submit-date-range", {
+      // Replace with your server endpoint URL
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dateRange),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        setServerResponse(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const calendarStyles = {
+    backgroundColor: "lightblue",
+    color: "white",
+    border: "2px solid red",
+    borderRadius: "4px",
+  };
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={["DateRangeCalendar"]}>
-        <DateRangeCalendar />
-      </DemoContainer>
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DateRangeCalendar"]}>
+          <div style={calendarStyles}>
+            <DateRangeCalendar
+              calendars={2}
+              value={dateRange}
+              onChange={handleDateRangeSelect}
+            />
+          </div>
+          {dateRange[0] !== null && dateRange[1] !== null && (
+            <button onClick={handleButtonClick}>Do Something</button>
+          )}
+        </DemoContainer>
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 }
