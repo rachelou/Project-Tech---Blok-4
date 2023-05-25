de1e58b69923bd65bad50c576ee8a9568f2e7529
Rachid Elouali <rachidelouali@re-byods-145-109-175-30.wireless.hva.nl>
2023-05-25 09:42:33 +0200

    API integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
 import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
 import { createTheme, ThemeProvider } from "@mui/material";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
-const theme = createTheme();
+const apiKey = "509917b15cee4c22b96205129232405";
+const location = "Amsterdam"; // Replace with the desired location
+const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4`;
 
 export default function BasicDateRangeCalendar() {
-  const [dateRange, setDateRange] = useState([null, null]);
-  const [serverResponse, setServerResponse] = useState("");
+  const [dateRange, setDateRange] = useState([null, null]); // State to store selected date range
+  const [serverResponse, setServerResponse] = useState(""); // State to store server response
+  const [weatherForecast, setWeatherForecast] = useState([]); // State to store weather forecast data
+
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const weatherData = response.data;
+        const forecastData = weatherData.forecast.forecastday.slice(1, 5); // Extract forecast data for the next 4 days
+        setWeatherForecast(forecastData); // Store the forecast data in state
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
   const handleDateRangeSelect = (range) => {
-    setDateRange(range);
+    setDateRange(range); // Update the date range state when a range is selected
   };
 
   const handleButtonClick = () => {
@@ -26,35 +44,109 @@ export default function BasicDateRangeCalendar() {
     })
       .then((response) => response.text())
       .then((data) => {
-        setServerResponse(data);
+        setServerResponse(data); // Store the server response in state
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
 
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Poppins", // Change the font family here
+    },
+    palette: {
+      primary: {
+        main: "#001DF5", // Customize the primary color
+      },
+      secondary: {
+        main: "#ffffff", // Customize the secondary color
+      },
+      // Add more customizations to the palette if needed
+    },
+  });
+
+  const containerStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
   const calendarStyles = {
-    backgroundColor: "lightblue",
-    color: "white",
-    border: "2px solid red",
-    borderRadius: "4px",
+    color: "#00000",
+    backgroundColor: "#ffffff",
+    borderRadius: "2rem",
+    boxShadow: "0 0 6px rgba(0, 0, 0, 0.1)",
+    margin: "1rem",
+    marginBottom: "2rem", // Add margin bottom of 2rem
+    borderRight: "none",
+  };
+
+  const buttonContainerStyles = {
+    display: "flex",
+    justifyContent: "flex-end",
+    margin: "2rem",
+  };
+
+  const buttonStyles = {
+    width: "fit-content",
+    padding: "0.5rem 1rem",
+  };
+
+  const weatherAreaStyles = {
+    margin: "2rem", // Add margin of 2rem
   };
 
   return (
     <ThemeProvider theme={theme}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={["DateRangeCalendar"]}>
-          <div style={calendarStyles}>
-            <DateRangeCalendar
-              calendars={2}
-              value={dateRange}
-              onChange={handleDateRangeSelect}
-            />
-          </div>
-          {dateRange[0] !== null && dateRange[1] !== null && (
-            <button onClick={handleButtonClick}>Do Something</button>
-          )}
-        </DemoContainer>
+        <div style={containerStyles}>
+          <DemoContainer components={["DateRangeCalendar"]}>
+            <center>
+              <h1>Fancy Datepicker 💫 </h1>{" "}
+            </center>
+            <div style={calendarStyles}>
+              <DateRangeCalendar
+                calendars={2}
+                value={dateRange}
+                onChange={handleDateRangeSelect}
+              />
+              {dateRange[0] !== null && dateRange[1] !== null && (
+                <div style={buttonContainerStyles}>
+                  <Button
+                    variant="contained"
+                    onClick={handleButtonClick}
+                    style={buttonStyles}
+                  >
+                    Reserve
+                  </Button>
+                </div>
+              )}
+              {weatherForecast.length > 0 && (
+                <div style={weatherAreaStyles}>
+                  <h3>☀️ Weather Forecast</h3>
+                  <ul style={{ listStyle: "none", padding: 0 }}>
+                    {weatherForecast.map((day, index) => (
+                      <li key={day.date}>
+                        <strong>
+                          {index === 0
+                            ? "Today"
+                            : index === 1
+                            ? "Tomorrow"
+                            : index === 2
+                            ? "The Day After Tomorrow"
+                            : ""}
+                        </strong>
+                        : {day.day.condition.text}, {day.day.avgtemp_c}°C
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </DemoContainer>
+        </div>
       </LocalizationProvider>
     </ThemeProvider>
   );
